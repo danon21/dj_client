8e664a338be4cff5569a5a83916f9e781a49c378
Bafomet <leousovs@gmail.com>
2023-04-28 11:03:13 +0500

    модифицировал логин

--- dj_client/src/views/Login.vue
+++ dj_client/src/views/Login.vue
@@ -2,28 +2,47 @@
   <div class="px-4 py-5 my-5 text-center">
     <div class="px-4 py-1 my-5 text-center">
       <img class="mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="100" height="100">
-      <form class="center-form" method="post" id="auth">
+      
+      <form class="center-form" method="post" id="auth" action=loadUser()>
         <h1 class="form-header">Dungeons & Dranics</h1>
-        <label class="form-question">Email address</label>
+        <label class="form-question">Login</label>
         <div>
-          <input name="login" id="login" type="email" placeholder="email@example.com" size="30" maxlength="100">
+          <input name="name" id="login" type="text" placeholder="baaf" size="30" maxlength="100">
         </div>
         <label class="form-question">Password</label>
         <div>
-          <input name="pass" id="pass" type="password" size="30" maxlength="100">
+          <input name="password" id="pass" type="password" size="30" maxlength="100">
         </div>
-        <input id="input" class="btn btn-primary my-2" type="submit" value="login">
+        <input id="input" class="btn btn-primary my-2" type="submit" value="Send">
         <div>
           <a class="btn btn-secondary my-2" href="#" id="href1">sign up</a>
         </div>
       </form>
+
+
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: `Login`
+  name: `Login`,
+  data() {
+    return {
+      user: {}
+    }
+  },
+  created() {
+
+  },
+  methods: {
+    async loadUser() {
+      this.user = await fetch(
+          `${this.$store.getters.getServerUrl}/authorization/sendd`
+      ).then(response => response.json())
+      console.log(this.user)
+    }
+  }
 }
 </script>
 
